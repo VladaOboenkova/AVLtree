51624d2b82b45cd8ade72de8da30ee966ffc1c26
Влада <vladaoboenkova@gmail.com>
2019-12-26 11:30:56 +0300

    added rotations and balance factor

--- src/main/java/AVLTree.java
+++ src/main/java/AVLTree.java
@@ -1,62 +1,66 @@
-import java.util.Collection;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class AVLTree<Key extends Comparable<Key>, Value>implements Map{
+import java.util.*;
 
-    private class Node<T>{
+public class AVLTree<Key extends Comparable<Key>, Value> extends AbstractMap<Key, Value> {
+
+    class Node<Key> {
         Key key;
         Value value;
-        int height;
-        private int size;
-        Node<T> left;
-        Node<T> right;
+        Node<Key> left = null;
+        Node<Key> right = null;
 
-        Node(Key key, Value value, int size) {
+        Node(Key key, Value value) {
             this.key = key;
             this.value = value;
-            this.size = size;
         }
     }
 
     private Node<Key> root = null;
 
+    private int size = 0;
+
+    @Override
     public int size() {
-        return size(root);
+        return size;
     }
 
-    private int size(Node node) {
-        if (node == null) return 0;
-        else return node.size;
-    }
-
-    @SuppressWarnings("unchecked")
-    private int balanceFactor(Node node) {
+    private int balanceFactor(Node<Key> node) {
         return height(node.left) - height(node.right);
     }
 
-    @SuppressWarnings("unchecked")
-    private Node rightRotation(Node firstNode) {
-        Node secondNode = firstNode.left;
+    private Node<Key> balance(Node<Key> node) {
+        if (balanceFactor(node) > 1) {
+            Node<Key> leftSubTree = node.left;
+            if (balanceFactor(leftSubTree) > 0) node = rightRotation(node);
+            else if (balanceFactor(leftSubTree) < 0) {
+                leftSubTree = leftRotation(leftSubTree);
+                node.left = leftSubTree;
+                node = rightRotation(node);
+            }
+        } else if (balanceFactor(node) < -1) {
+            Node<Key> rightSubTree = node.right;
+            if (balanceFactor(rightSubTree) < 0) node = leftRotation(node);
+            else if (balanceFactor(rightSubTree) > 0) {
+                rightSubTree = rightRotation(rightSubTree);
+                node.right = rightSubTree;
+                node = leftRotation(node);
+            }
+        }
+        return node;
+    }
+
+    private Node<Key> rightRotation(Node<Key> firstNode) {
+        Node<Key> secondNode = firstNode.left;
         firstNode.left = secondNode.right;
         secondNode.right = firstNode;
-        secondNode.size = firstNode.size;
-        firstNode.size = 1 + size(firstNode.left) + size(firstNode.right);
-        firstNode.height = 1 + Math.max(height(firstNode.left), height(firstNode.right));
-        secondNode.height = 1 + Math.max(height(secondNode.left), height(secondNode.right));
         return secondNode;
     }
 
-    @SuppressWarnings("unchecked")
-    private Node leftRotation(Node firstNode) {
-        Node secondNode = firstNode.right;
+    private Node<Key> leftRotation(Node<Key> firstNode) {
+        Node<Key> secondNode = firstNode.right;
         firstNode.right = secondNode.left;
         secondNode.left = firstNode;
-        secondNode.size = firstNode.size;
-        firstNode.size = 1 + size(firstNode.left) + size(firstNode.right);
-        firstNode.height = 1 + Math.max(height(firstNode.left), height(firstNode.right));
-        secondNode.height = 1 + Math.max(height(secondNode.left), height(secondNode.right));
         return secondNode;
     }
 
@@ -64,145 +68,182 @@ public class AVLTree<Key extends Comparable<Key>, Value>implements Map{
         return height(root);
     }
 
-    @SuppressWarnings("unchecked")
-    private int height(Node node) {
-        if (node == null) return -1;
+    private int height(Node<Key> node) {
+        if (node == null) return 0;
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
-    public boolean isEmpty() {
-        return size() == 0;
-    }
-
-    public boolean containsKey(Object key) {
-        if (key == null) throw new IllegalArgumentException();
-        return get(key) != null;
-    }
-
-    public boolean containsValue(Object value) { //!!!!
-        return false;
-    }
-
     @SuppressWarnings("unchecked")
-    public Object get(Object key) {
-        return get(root, (Key) key);
+    @Override
+    public Value get(Object key) {
+        if (root == null) return null;
+        Key k = (Key) key;
+        return get(root, k);
     }
 
-    @SuppressWarnings("unchecked")
-    private Object get(Node node, Key key) {
-        if (key == null) throw new IllegalArgumentException();
+    private Value get(Node<Key> node, Key key) {
         if (node == null) return null;
-        int compare = key.compareTo((Key) node.key);
-        if (compare < 0) return get(node.left, key);
-        else if (compare > 0) return get(node.right, key);
-        else return node.value;
+        int compare = key.compareTo(node.key);
+        if (compare == 0) return node.value;
+        else if (compare < 0) return get(node.left, key);
+        else return get(node.right, key);
     }
 
-    @SuppressWarnings("unchecked")
-    public Object put(Object key, Object value) {
-        if (key == null) throw new IllegalArgumentException();
-        return root = put(root, (Key) key, (Value) value);
+    public Node<Key> find(Key key) {
+        if (root == null) return null;
+        return find(root, key);
     }
 
-    @SuppressWarnings("unchecked")
-    private Node put(Node node, Key key, Value value) { // проверка баланса(!)
-        if (node == null) return new Node(key, value, 1);
-        int compare = key.compareTo((Key) node.key);
-        if (compare < 0) node.left  = put(node.left,  key, value);
-        else if (compare > 0) node.right = put(node.right, key, value);
-        else node.value = value;
-        node.size = 1 + size(node.left) + size(node.right);
-        return node;
-    }
-
-    @SuppressWarnings("unchecked")
-    public Object remove(Object key) {
-        if (key == null) throw new IllegalArgumentException();
-        return root = delete(root, (Key) key);
-    }
-
-    @SuppressWarnings("unchecked")
-    private Node delete(Node node, Key key) {
+    private Node<Key> find(Node<Key> node, Key key) {
+        if (root == null) return null;
         if (node == null) return null;
-        int compare = key.compareTo((Key) node.key);
-        if  (compare < 0) node.left  = delete(node.left,  key);
-        else if (compare > 0) node.right = delete(node.right, key);
-        else {
-            if (node.right == null) return node.left;
-            if (node.left  == null) return node.right;
-            Node t = node;
-            node = min(t.right);
-            node.right = deleteMin(t.right);
-            node.left = t.left;
+        int compare = key.compareTo(node.key);
+        if (compare == 0) return node;
+        else if (compare < 0) {
+            if (node.left == null) return node;
+            return find(node.left, key);
+        } else {
+            if (node.right == null) return node;
+            return find(node.right, key);
         }
-        node.size = size(node.left) + size(node.right) + 1;
-        return node;
+    }
+
+    public Value put(Key key, Value value) {
+        if (root == null) {
+            root = new Node<>(key, value);
+            size++;
+        }
+        root = put(root, key, value);
+        return value;
+    }
+
+    private Node<Key> put(Node<Key> node, Key key, Value value) {
+        Node<Key> n = new Node<>(key, value);
+        Node<Key> nodeA = find(root, key);
+        assert nodeA != null;
+        int compare = key.compareTo(nodeA.key);
+        if (compare == 0) nodeA.value = value;
+        else {
+            insert(n);
+            size++;
+        }
+        return balance(node);
+    }
+
+    private void insert(Node<Key> node) {
+        if (root == null) root = node;
+        else {
+            Node<Key> current = root;
+            Node<Key> parent;
+            int compare = node.key.compareTo(current.key);
+            while (true) {
+                parent = current;
+                if (compare < 0) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = node;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = node;
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
-    public Key min() {
-        if (isEmpty()) throw new NoSuchElementException();
-        return (Key) min(root).key;
+    public Value remove(Object key) {
+        Value value = get(key);
+        root = remove(root, (Key) key);
+        return value;
     }
 
-    @SuppressWarnings("unchecked")
-    private Node min(Node node) {
-        if (node.left == null) return node;
-        else return min(node.left);
+    private Node<Key> remove(Node<Key> node, Key key) {
+        if (root == null) return null;
+        Node<Key> current = root;
+        Node<Key> parent = root;
+        boolean isLeft = true;
+
+        while (current.key != key) {
+            parent = current;
+            if (key.compareTo(current.key) < 0) {
+                isLeft = true;
+                current = current.left;
+            } else {
+                isLeft = false;
+                current = current.right;
+            }
+            if (current == null)
+                return null;
+        }
+
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeft) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        } else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else if (isLeft) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        } else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else if (isLeft) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        } else {
+            Node<Key> successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeft) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+        size--;
+        return balance(current);
     }
 
-    @SuppressWarnings("unchecked")
-    public void deleteMin() {
-        if (isEmpty()) throw new NoSuchElementException();
-        root = deleteMin(root);
-    }
 
-    @SuppressWarnings("unchecked")
-    private Node deleteMin(Node node) {
-        if (node.left == null) return node.right;
-        node.left = deleteMin(node.left);
-        node.size = size(node.left) + size(node.right) + 1;
-        return node;
-    }
-
-    @SuppressWarnings("unchecked")
-    public void deleteMax() {
-        if (isEmpty()) throw new NoSuchElementException();
-        root = deleteMax(root);
-    }
-
-    @SuppressWarnings("unchecked")
-    private Node deleteMax(Node node) {
-        if (node.right == null) return node.left;
-        node.right = deleteMax(node.right);
-        node.size = size(node.left) + size(node.right) + 1;
-        return node;
+    private Node<Key> getSuccessor(Node<Key> delNode) {
+        Node<Key> successorParent = delNode;
+        Node<Key> successor = delNode;
+        Node<Key> current = delNode.right;
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.left;
+        }
+        if (successor != delNode.right) {
+            successorParent.left = successor.right;
+            successor.right = delNode.right;
+        }
+        return successor;
     }
 
     public void clear() {
         root = null;
     }
 
-    public void putAll(Map m) {
-
-    }
-
-    public Set keySet() {
-        return null;
-    }
-
-    public Collection values() {
-        return null;
-    }
-
-    public Set<Entry> entrySet() {
-        return null;
-    }
-
-    public static void main(String[] args){
-        AVLTree<Integer, String> tree = new AVLTree<Integer, String>();
-        tree.put(3, "Map");
-        System.out.println(tree.get(3));
-        System.out.println(tree.size());
+    public Set<Entry<Key, Value>> entrySet() {
+        throw new NotImplementedException();
     }
 }
+
+
